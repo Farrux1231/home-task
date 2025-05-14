0eed4b32843b15612c31655e012adb3846a2f69a
Farrukh_Sanakulov <farrukhmeyliyev001@gmail.com>
2025-05-14 12:20:24 +0500

    fixed brows bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
-import Sidebar from './components/sideBar/SideBar.jsx'
-import Create from './pages/create/Create.jsx'
-import Delete from './pages/delete/Delete.jsx'
-import Read from './pages/read/Read.jsx'
-import { Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sidebar from './components/sideBar/SideBar';
+import Create from './pages/create/Create';
+import Delete from './pages/delete/Delete';
+import Read from './pages/read/Read';
 
 function App() {
-
   return (
-    <>
-       <Router>
-      <div className="flex">
-        <Sidebar />
-        <div className="flex-1">
-          <Routes>
-            <Route path="/create" element={<Create />} />
-            <Route path="/delete" element={<Delete />} />
-            <Route path="/read" element={<Read />} />
-          </Routes>
+    <main>
+      <Router>
+        <div className="flex">
+          <Sidebar />
+          <div className="flex-1">
+            <Routes>
+              <Route path="/create" element={<Create />} />
+              <Route path="/delete" element={<Delete />} />
+              <Route path="/read" element={<Read />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </Router>
-    </>
-  )
+      </Router>
+    </main>
+  );
 }
 
-export default App
+export default App;
